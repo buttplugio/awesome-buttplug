52817196affb7fb174778a27962a0c6482807a84
Kyle Machulis <kyle@nonpolynomial.com>
2026-05-30 18:49:18 -0700

    fix: clean markdown from metadata display

---
+++ src/utils/displayText.ts
@@ -0,0 +1,12 @@
+export function stripInlineMarkdown(value: string): string {
+  return value
+    .replace(/!\[([^\]]*)\]\([^)]+\)/g, "$1")
+    .replace(/\[([^\]]+)\]\([^)]+\)/g, "$1")
+    .replace(/`([^`]+)`/g, "$1")
+    .replace(/\*\*([^*]+)\*\*/g, "$1")
+    .replace(/\*([^*]+)\*/g, "$1")
+    .replace(/__([^_]+)__/g, "$1")
+    .replace(/_([^_]+)_/g, "$1")
+    .replace(/\s+/g, " ")
+    .trim();
+}
